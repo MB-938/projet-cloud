233021aa059cc80a51cb89579e8ec1e28456e859
Mohamed <bengueddache.mohamed@gmail.com>
2026-06-18 09:50:43 +0200

    feat(k8s): Angular frontend containerisé et déployé sur Minikube

---
+++ partie2-apps/hello-angular/src/app/app.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+import { App } from './app';
+
+describe('App', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(App);
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+
+  it('should have correct message', () => {
+    const fixture = TestBed.createComponent(App);
+    expect(fixture.componentInstance.message())
+      .toEqual('Hello World depuis Angular !');
+  });
+});
---
+++ partie2-apps/hello-angular/src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  public readonly message = signal('Hello World depuis Angular !');
+}
